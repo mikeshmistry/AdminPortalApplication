4d7e245b62066729f168fee914d2d531319d0111
Happy user <Happy user@DESKTOP-C8BUOD4>
2021-01-11 22:02:45 -0500

    Add project files.

---
+++ AdminPortal.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdminPortal.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
